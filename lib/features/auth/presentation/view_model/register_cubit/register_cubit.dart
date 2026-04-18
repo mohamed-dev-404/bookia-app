45ebab2afd23dfd1d6dfb44ff06ff6f2f8ef57da
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-04-18 05:27:09 +0200

    feat(auth): init RegisterCubit and RegisterState for registration view and its states management.

---
+++ lib/features/auth/presentation/view_model/register_cubit/register_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bookia/features/auth/data/repo/auth_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  final AuthRepo authRepo;
+  RegisterCubit({required this.authRepo}) : super(RegisterInitial());
+}
