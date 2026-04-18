5069b12c3b0c45750cf2de1add9c13ab94bfbc73
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-04-18 05:28:05 +0200

    feat(auth): register AuthRepo, LoginCubit, RegisterCubit, and ForgetPasswordCubit in service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,9 +1,14 @@
 import 'package:bookia/core/network/api_consumer.dart';
 import 'package:bookia/core/network/dio_consumer.dart';
+import 'package:bookia/features/auth/data/repo/auth_repo.dart';
+import 'package:bookia/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:bookia/features/auth/presentation/view_model/forget_password_cubit/forget_password_cubit.dart';
+import 'package:bookia/features/auth/presentation/view_model/login_cubit/login_cubit.dart';
+import 'package:bookia/features/auth/presentation/view_model/register_cubit/register_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-/// This file is responsible for registering all the services 
+/// This file is responsible for registering all the services
 /// that will be used in the app using GetIt package for [dependency_injection].
 final GetIt getIt = GetIt.instance;
 
@@ -15,7 +20,23 @@ void setupServiceLocator() {
 
   //! Auth Feature
 
-  // getIt.registerLazySingleton<AuthRepo>(
-  //   () => AuthRepoImpl(getIt<ApiConsumer>()),
-  // );
+  //? Auth Repo
+  getIt.registerLazySingleton<AuthRepo>(
+    () => AuthRepoImpl(getIt<ApiConsumer>()),
+  );
+
+  //? Login Cubit
+  getIt.registerLazySingleton<LoginCubit>(
+    () => LoginCubit(authRepo: getIt<AuthRepo>()),
+  );
+
+  //? Register Cubit
+  getIt.registerLazySingleton<RegisterCubit>(
+    () => RegisterCubit(authRepo: getIt<AuthRepo>()),
+  );
+
+  //? Forget Password Cubit
+  getIt.registerLazySingleton<ForgetPasswordCubit>(
+    () => ForgetPasswordCubit(authRepo: getIt<AuthRepo>()),
+  );
 }
